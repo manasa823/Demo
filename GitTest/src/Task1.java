4401a6710e0886307bd1d0c60b9e2866c4a8c62b
Manasa <manasachannamaraju@gmail.com>
2021-12-22 12:52:12 +0530

    Test merge

--- GitTest/src/Task1.java
+++ GitTest/src/Task1.java
@@ -4,5 +4,6 @@ public class Task1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Now merging a new feature");
+		System.out.println("now trying merge");
 	}
 }
